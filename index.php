cbfffafd8971131af53cd81b564174a84cf51786
Kinji_Kamizaki <Kinji.Kamizaki@gmail.com>
2021-01-30 13:17:20 +0900

    timeout extend

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 $xml = curl_exec($ch);
 curl_close($ch);
 
